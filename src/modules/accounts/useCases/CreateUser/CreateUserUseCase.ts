80ebc249b91deae93ef3ff593e9cbf1fe6460955
AndreMotta25 <andremotta16041998@gmail.com>
2023-02-13 03:09:27 -0300

    update: Sending email when user registers

--- src/modules/accounts/useCases/CreateUser/CreateUserUseCase.ts
+++ src/modules/accounts/useCases/CreateUser/CreateUserUseCase.ts
@@ -1,15 +1,23 @@
 import { hash } from 'bcryptjs';
+import { sign } from 'jsonwebtoken';
 import normalizeEmail from 'normalize-email';
 import { injectable, inject } from 'tsyringe';
 
+import { IEmailSender } from '../../../../services/Email/IEmailSender';
+import { convertTime } from '../../../../utils/convertTime';
 import { IRequest, IUserRepository } from '../../repositories/IUserRepository';
 
 @injectable()
 class CreateUserUseCase {
   private readonly repository: IUserRepository;
+  private readonly sender: IEmailSender;
 
-  constructor(@inject('IUserRepository') repository: IUserRepository) {
+  constructor(
+    @inject('IUserRepository') repository: IUserRepository,
+    @inject('IEmailSender') sender: IEmailSender
+  ) {
     this.repository = repository;
+    this.sender = sender;
   }
 
   async execute({ username, email, password }: IRequest) {
@@ -25,10 +33,25 @@ class CreateUserUseCase {
 
     const passwordHash = await hash(password, 8);
 
-    await this.repository.create({
+    const user = await this.repository.create({
       username,
       email: normalizedEmail,
       password: passwordHash,
+    });
+
+    const token = sign(
+      { subject: user.id, exp: convertTime.toMin(10) },
+      user.hashToken
+    );
+
+    this.sender.config({ host: 'smtp.gmail.com', port: 465 });
+
+    this.sender.send({
+      target: normalizedEmail,
+      subject: 'Confirmação de email',
+      message: token,
+      username: user.username,
+      template: 'confirmEmail',
     });
   }
 }
