5805a3c98c0101bc1c7d786c6f5c5024bb5c04b1
AndreMotta25 <andremotta16041998@gmail.com>
2023-02-13 01:12:21 -0300

    feat: configuring multer

---
+++ src/config/upload.ts
@@ -0,0 +1,31 @@
+import crypto from 'crypto';
+import { Request } from 'express';
+import multer from 'multer';
+
+export const upload = (file: string) => {
+  return {
+    storage: multer.diskStorage({
+      destination: file,
+      filename: (req, file, callback) => {
+        const hex = crypto.randomBytes(16).toString('hex');
+        callback(null, `${hex}-${file.originalname}`);
+      },
+    }),
+  };
+};
+
+export const fileFilter = (
+  req: Request,
+  file: Express.Multer.File,
+  cb: multer.FileFilterCallback
+) => {
+  if (
+    !file.originalname.endsWith('.jpg') &&
+    !file.originalname.endsWith('.png')
+  ) {
+    console.log('não é uma foto');
+    return cb(null, false);
+  }
+  console.log('é uma foto');
+  return cb(null, true);
+};
