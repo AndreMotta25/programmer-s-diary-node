90926ed263f7fb6feabf8c3ab89e3d9fd81e006a
AndreMotta25 <andremotta16041998@gmail.com>
2023-03-05 18:07:12 -0300

    :sparkles: feat: injecting cardRepository

--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -1,7 +1,9 @@
 import { container } from 'tsyringe';
 
 import { UserRepository } from '../../modules/accounts/repositories/typeorm/UserRepository';
+import { CardRepository } from '../../modules/cards/repositories/typeorm/CardRepository';
 import { EmailSend } from '../../services/Email/EmailSender';
 
 container.registerSingleton('IUserRepository', UserRepository);
+container.registerSingleton('ICardRepository', CardRepository);
 container.register('IEmailSender', EmailSend);
