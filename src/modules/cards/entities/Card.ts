14de0b51af481b50b6e12662ddda5f67fa1a2065
AndreMotta25 <andremotta16041998@gmail.com>
2023-03-05 18:04:55 -0300

    :sparkles: feat: entity card

---
+++ src/modules/cards/entities/Card.ts
@@ -0,0 +1,40 @@
+import {
+  CreateDateColumn,
+  UpdateDateColumn,
+  Column,
+  PrimaryColumn,
+  Entity,
+  ManyToOne,
+} from 'typeorm';
+import { v4 as uuidV4 } from 'uuid';
+
+import { User } from '../../accounts/entities/User';
+
+@Entity()
+class Card {
+  @PrimaryColumn()
+  id: string;
+  @Column()
+  name: string;
+  @Column()
+  language: string;
+  @Column({ type: 'text' })
+  description: string;
+  @Column({ type: 'text' })
+  code: string;
+
+  @CreateDateColumn()
+  create_date: Date;
+
+  @UpdateDateColumn()
+  update_date: Date;
+
+  @ManyToOne(() => User, (user) => user.cards, { onDelete: 'CASCADE' })
+  user: User;
+
+  constructor() {
+    this.id = uuidV4();
+  }
+}
+
+export { Card };
