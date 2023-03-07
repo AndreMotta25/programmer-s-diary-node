89d22b6cbb0cfc92092804e1ef90f0bc8272dae8
AndreMotta25 <andremotta16041998@gmail.com>
2023-03-07 18:26:03 -0300

    :sparkles: feat: preparing to deploy

--- src/database/index.ts
+++ src/database/index.ts
@@ -2,10 +2,11 @@ import { config } from 'dotenv';
 import { DataSource } from 'typeorm';
 
 config();
+
 const appDataSource = new DataSource({
   type: 'postgres',
   host: process.env.DB_HOST as string,
-  port: 9000,
+  port: Number(process.env.DB_PORT as string) || 9000,
   username: process.env.DB_USER as string,
   password: process.env.DB_PASS as string,
   database: process.env.DB_NAME as string,
